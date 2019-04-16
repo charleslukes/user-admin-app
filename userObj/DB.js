f67e995ac0407826d9db6bba17b8da94a7ddcad4
Charles Chiakwa <charleslukes28@gmail.com>
2019-04-16 11:30:09 +0100

    Add the constructor in the Amin file

---
+++ userObj/DB.js
@@ -0,0 +1,5 @@
+const DB = [
+
+];
+
+module.exports = DB;
---
+++ userObj/admin.js
@@ -0,0 +1,11 @@
+const DB = require('./DB');
+
+function Admin(name){
+    this.name = name;
+}
+
+Admin.prototype.deleteAll = function(){
+    return DB = [];
+}
+
+const admin = new Admin('Uche');
