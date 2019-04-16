bf088a7378b641fb05e9e476370068a77ba74c4f
Charles Chiakwa <charleslukes28@gmail.com>
2019-04-16 09:43:02 +0100

    Create user function added

--- userObj/user.js
+++ userObj/user.js
@@ -3,6 +3,22 @@
 
 const DB = [];
 
+//Creating the user constructor 
+function createUser(name, surname, state){
+    this.name = name;
+    this.surname = surname;
+    this.state = state;
+
+    //Store added values to the DataBase
+    const store = {};
+    store.name = name;
+    store.surname = surname;
+    store.state = state;
+
+    DB.push(store);
+}
+
 module.exports = {
-    DB: DB
+    DB: DB,
+    createUser: createUser
 }
--- userObj/user.test.js
+++ userObj/user.test.js
@@ -1,11 +1,17 @@
-const {DB} = require('./user')
+const {DB, createUser} = require('./user')
 
 
-describe('Database to be an array of objects', () => {
+describe('Database of the user', () => {
     it('Should be defined', () => {
         expect(DB).toBeDefined();
     })
     it('Should be an array ', () => {
         expect(DB).toEqual(expect.arrayContaining(DB))
+    })
+})
+
+describe('The create user function ', () => {
+    it('Should be defined ', () => {
+        expect(createUser('xyz', 'xyz', 'xyz')).toBeDefined();
     })
 })
