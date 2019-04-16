572cc9562706467485a2077b93fe3a572a3a699a
Charles Chiakwa <charleslukes28@gmail.com>
2019-04-16 09:32:50 +0100

    Add test to check if DataBase is an array

--- userObj/user.test.js
+++ userObj/user.test.js
@@ -2,7 +2,10 @@ const {DB} = require('./user')
 
 
 describe('Database to be an array of objects', () => {
-    it('Should be an array', () => {
+    it('Should be defined', () => {
         expect(DB).toBeDefined();
+    })
+    it('Should be an array ', () => {
+        expect(DB).toEqual(expect.arrayContaining(DB))
     })
 })
